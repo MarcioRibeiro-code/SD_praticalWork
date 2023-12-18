cac66611b671d9dfe7e4a4ca4742f4b16d59c836
Hugo <ribeirohugo9400@gmail.com>
2023-12-18 18:56:09 +0000

    Create Central Node

---
+++ app/src/main/java/LocalNode/LocalNode.java
@@ -0,0 +1,36 @@
+package LocalNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocalNode {
+    private final String name;
+    private final String password;
+    private final String id;
+
+    //Make sense store Channels there? I will store in LocalNodeLogin
+    //private final List<String> channels;
+
+
+
+
+    public LocalNode(String name, String password, String id, List<String> channels) {
+        this.name = name;
+        this.password = password;
+        this.id = id;
+        //this.channels = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+}
