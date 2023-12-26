6d61eb7de130ff54b469b0412e1012984ce574ef
Hugo <ribeirohugo9400@gmail.com>
2023-12-26 12:20:37 +0000

    Created Graphic Interface for Register

---
+++ app/src/main/java/GUI/register.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import javax.swing.*;
+
+public class register {
+    private JButton registerButton;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+}
